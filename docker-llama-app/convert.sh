1ed5fbaad2ba8052d4468ddd2d518087403866b1
Ubuntu <ubuntu@ip-172-31-33-86.ap-northeast-2.compute.internal>
2024-09-10 00:27:33 +0000

    scripts

---
+++ docker-llama-app/convert.sh
@@ -0,0 +1,3 @@
+python3 $HOME/llama.cpp/convert_hf_to_gguf.py \
+	$HOME/models/Meta-Llama-3.1-8B-Instruct \
+	--outtype q8_0 
---
+++ docker-llama-app/llama.cpp.gguf.infer.sh
@@ -0,0 +1,6 @@
+$HOME/llama.cpp/llama-cli \
+	-m $HOME/models/gemma-2-2b-it/gemma-2-2B-it-Q5_K_S.gguf \
+	-p "Please tell me about Docker in 10 sentences." \
+    -n 400 \
+	-e \
+	--log-disable
